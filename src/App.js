801ec3fcc29b8255745df0aa9ee91ea91c7170ba
Radi Totev <raditotev@gmail.com>
2021-04-21 10:01:38 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react'
+import {useAsync} from 'utils/hooks'
+import GoogleMapReact from 'google-map-react'
+import {FaGripfire} from 'react-icons/fa'
+import {FaSpinner} from 'react-icons/fa'
 
-function App() {
+const fetch = require('node-fetch')
+const FireIcon = props => <FaGripfire className="icon" />
+function FullPageSpinner() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="loading">
+      <FaSpinner className="spinner" />
+      <h1>Loading...</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+function App() {
+  const {
+    isLoading,
+    data: {events},
+    run,
+  } = useAsync({data: []})
+
+  useEffect(() => {
+    run(
+      fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/8').then(res =>
+        res.json(),
+      ),
+    )
+  }, [run])
+
+  if (isLoading) {
+    return <FullPageSpinner />
+  }
+
+  return (
+    <GoogleMapReact
+      bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_API_KEY}}
+      defaultCenter={{
+        lat: -33.10204,
+        lng: -71.49142,
+      }}
+      defaultZoom={5}
+    >
+      {events?.map(event => {
+        const [lng, lat] = event.geometries[0].coordinates
+        return <FireIcon key={lat} lat={lat} lng={lng} />
+      })}
+    </GoogleMapReact>
+  )
+}
+
+export default App
