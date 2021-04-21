2394322e9e740086eb359ddfe62a86934aeb0cc2
Radi Totev <raditotev@gmail.com>
2021-04-21 11:23:17 +0300

    Add application header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import {Map} from 'components/map'
+import {Header} from 'components/header'
+import {Fragment} from 'react'
 
 function App() {
-  const {
-    isLoading,
-    data: {events},
-    run,
-  } = useAsync({data: []})
-
-  useEffect(() => {
-    run(
-      fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/8').then(res =>
-        res.json(),
-      ),
-    )
-  }, [run])
-
-  if (isLoading) {
-    return <FullPageSpinner />
-  }
-
   return (
+    <Fragment>
+      <Header />
       <Map />
+    </Fragment>
   )
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Earth Natural Events</h1>
+    </header>
+  )
+}
+
+export {Header}
