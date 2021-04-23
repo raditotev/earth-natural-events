adbd6175cd4a9bf2d5d0120556ece750b993b4a1
Radi Totev <raditotev@gmail.com>
2021-04-23 18:06:25 +0300

    Add module mapping icons to event types

---
+++ src/utils/category-icons.js
@@ -0,0 +1,27 @@
+import {FaThermometerHalf} from 'react-icons/fa'
+import {GiEruption} from 'react-icons/gi'
+import {GiTumbleweed} from 'react-icons/gi'
+import {GiDustCloud} from 'react-icons/gi'
+import {GiFallingRocks} from 'react-icons/gi'
+import {GiBulldozer} from 'react-icons/gi'
+import {GiIceberg} from 'react-icons/gi'
+import {GiSnowflake2} from 'react-icons/gi'
+import {ImFire} from 'react-icons/im'
+import {RiEarthquakeFill} from 'react-icons/ri'
+import {RiFloodFill} from 'react-icons/ri'
+import {BsDropletHalf} from 'react-icons/bs'
+
+export const categoryIcons = {
+  6: <GiTumbleweed />,
+  7: <GiDustCloud />,
+  16: <RiEarthquakeFill />,
+  9: <RiFloodFill />,
+  14: <GiFallingRocks />,
+  19: <GiBulldozer />,
+  15: <GiIceberg />,
+  17: <GiSnowflake2 />,
+  18: <FaThermometerHalf />,
+  12: <GiEruption />,
+  13: <BsDropletHalf />,
+  8: <ImFire />,
+}
