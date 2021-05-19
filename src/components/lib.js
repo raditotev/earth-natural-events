31340a59ed157752db0c35a495dbfe1923517542
Radi Totev <raditotev@gmail.com>
2021-05-19 09:23:58 +0300

    Add component for error displaying

--- src/components/lib.js
+++ src/components/lib.js
@@ -45,4 +45,23 @@ function FullPageSpinner() {
   )
 }
 
-export {FullPageSpinner}
+function FullPageErrorFallback({error}) {
+  return (
+    <div
+      role="alert"
+      css={{
+        color: 'red',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      <pre>{error.message}</pre>
+    </div>
+  )
+}
+
+export {FullPageSpinner, FullPageErrorFallback}
