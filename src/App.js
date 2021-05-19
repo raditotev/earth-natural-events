df0dbb05c4ae4c0391a32aac984f4e691d9af5b0
Radi Totev <raditotev@gmail.com>
2021-05-19 09:27:07 +0300

    Intercept any app errors and display in user friendly format

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import {useState} from 'react'
+import {ErrorBoundary} from 'react-error-boundary'
 import {Map} from 'components/map'
 import {Header} from 'components/header'
-import {Fragment, useState} from 'react'
+import {FullPageErrorFallback} from 'components/lib'
 
 function App() {
   const [events, setEvents] = useState([])
 
   return (
-    <Fragment>
+    <ErrorBoundary FallbackComponent={FullPageErrorFallback}>
       <Header setEvents={setEvents} />
       <Map events={events} />
-    </Fragment>
+    </ErrorBoundary>
   )
 }
 
